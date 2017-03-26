b5a7f32376c8e0b24ec85945c3b093c92639d90c
Vinh Nguyen <kurei@axcoto.com>
2017-03-26 13:11:22 -0700

    Use a local jsob db

--- shot.js
+++ shot.js
@@ -1,17 +1,56 @@
 const webshot = require('webshot')
 const readline = require('readline')
 const stream = require('stream')
+const async   = require('async')
+const db = require('lowdb')('db.json')
 
 const fs = require('fs')
 const instream = fs.createReadStream('./urls')
 const outstream = new stream
 const rl = readline.createInterface(instream, outstream)
+let queue = []
+
+function initDb() {
+  db.defaults({ sites: [] }).write()
+}
 
 rl.on('line', url => {
-  console.log(url)
+  const filename = url.replace(/[^\w]/g, "-")
+  if (filename == "") {
+    return
+  }
 
-  webshot('google.com', `${url}.png`, function(err) {
-    console.log("Fail to process", err)
+  queue.push(url)
+  console.log('Enqueue', url)
+})
+
+rl.on('close', () => {
+  console.log("Processing")
+  async.eachLimit(queue, 10, (url, next) => {
+    const filename = url.replace(/[^\w]/g, "-")
+    console.log("Doing", url)
+    webshot(url, `shots/${filename}.png`, {
+      phantomConfig: {
+        "ssl-protocol": "any",
+        "ignore-ssl-errors": "yes",
+      }
+    }, (err) => {
+      if (err) {
+        console.log("Error when take", url, err)
+      } else {
+        db.get('sites')
+          .push({id: filename, src: filename})
+          .write()
+      }
+      next()
+    })
+  }, (err, result) => {
+    if (err) {
+      console.log("Get error", err)
+      return
+    }
+
+    console.log("Done")
   })
 })
 
